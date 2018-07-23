cc8fd5619ba187b7413531b237a7376dbbe3bbdc
sendmenas <giedrius.bielskis@gmail.com>
2018-07-23 23:39:51 +0300

    Main and Footer
    Main - no logic yet
    Footer - time sync and some more

--- src/controllers/Footer.js
+++ src/controllers/Footer.js
@@ -1,11 +1,116 @@
 import React, { Component } from 'react';
 import '../css/Footer.css';
 
+function getFullNumberString(nr) {
+    return nr < 10 ? "0" + nr : nr;
+}
+
 class Footer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            syncTime: "Not synced",
+            items: null,
+        }
+    }
+
+    setSyncTime() {
+        let date = new Date();
+        let year = date.getFullYear();
+        let month = getFullNumberString(date.getMonth() + 1);
+        let day = getFullNumberString(date.getDate());
+        let hours = getFullNumberString(date.getHours());
+        let minutes = getFullNumberString(date.getMinutes());
+        let seconds = getFullNumberString(date.getSeconds());
+        let fullDate = year + "-" + month + "-" + day + " " + hours + ":" + minutes + ":" + seconds;
+        this.setState({
+            syncTime: fullDate,
+        });
+    }
+
+    componentDidMount() {
+        fetch('https://raw.githubusercontent.com/sendmenas/MBS_Contactify/master/contacts.json')
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    this.setState({
+                        items: result,
+                    });
+                    this.props.setDataItems(result);
+                },
+                (error) => {
+                    this.setState({
+                        error
+                    });
+                }
+            )
+    }
+
     render() {
+        
         return (
-            <div>
-            </div>
+            <footer className="footer">
+                <nav className="footer__left">
+                    <ul className="left-links">
+                        <li>
+                            <a href="">Dashboard</a>
+                        </li>
+                        <li>
+                            <a href="">Contacts</a>
+                        </li>
+                        <li>
+                            <a href="">Notifications</a>
+                        </li>
+                    </ul>
+                    <nav className="left-copyright">
+                        <div className="copyright-label">&copy; 2015 Contactify</div>
+                        <ul className="left-links-bottom">
+                            <li>
+                                <a href="">About</a>
+                            </li>
+                            <li>
+                                <a href="">Privacy</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </nav>
+                <div className="footer__center">
+                    <div className="top-box">
+                        <div className="top-box__icon"></div>
+                        <div>
+                            <div>Last synced:</div>
+                            <div>{this.state.syncTime}</div>
+                        </div>
+                        <div className="top-box__sync-container" onClick={() => this.setSyncTime()}>
+                            <div className="sync-icon"></div>
+                            <div>Force sync</div>
+                        </div>
+                    </div>
+                    <div className="bottom-box">
+                        <div className="bottom-box__icon"></div>
+                        <div>
+                            <div>Help Desk and Resolution center available:</div>
+                            <div>I-IV 8:00-18:00, V 8:00-16:45</div>
+                        </div>
+                    </div>
+                </div>
+                <nav className="footer__right">
+                    <ul className="right-links">
+                        <li>
+                            <a href="">Groups</a>
+                        </li>
+                        <li>
+                            <a href="">Frequently contacted</a>
+                        </li>
+                        <li>
+                            <a href="">Preferences</a>
+                        </li>
+                        <li>
+                            <a href="">Log out</a>
+                        </li>
+                    </ul>
+                </nav>
+            </footer>
         );
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
 import Header from './controllers/Header';
@@ -6,12 +6,38 @@ import Main from './controllers/Main';
 import Footer from './controllers/Footer';
 import registerServiceWorker from './registerServiceWorker';
 
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            items: null,
+            itemsReceived: false,
+        }
+    }
+    
+    setDataItems(data) {
+        console.log(data);
+    }
+    
+    render() {
+        let isLoaded = this.state.itemsReceived;
+
+        return (
+            <div className='page'>
+                <Header />
+                <Main 
+                    loaded={isLoaded}
+                />
+                <Footer 
+                    setDataItems={this.setDataItems}
+                    loaded={isLoaded}
+                />
+            </div>
+        );
+    }
+}
+
 ReactDOM.render(
-    <div className='page'>
-        <Header />
-        <Main />
-        <Footer />
-    </div>
-    ,
+    <App />,
     document.getElementById('root'));
 registerServiceWorker();
